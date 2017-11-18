3cadeaaf259a03ca9ebc3e1d02818b90d62654a6
kai li <adamlee516@gmail.com>
2017-11-18 13:52:31 -0500

    separate data model

--- js/data.js
+++ js/data.js
@@ -1,5 +1,47 @@
+// model
 var locations = [
-    {title: 'University of Montreal', location: {lat: 45.504543, lng: -73.613359}},
-    {title: 'Concordia University', location: {lat: 45.457843, lng: -73.641568}},
-    {title: 'Université du Québec à Montréal', location: {lat: 45.512884, lng:-73.558306}}
+    {
+        title: 'University of Montreal', 
+        location: 
+        {
+            lat: 45.504543,
+            lng: -73.613359
+        }
+    },
+
+    {
+        title: 'Concordia University',
+        location:
+        {
+            lat: 45.457467,
+            lng: -73.639222
+        }
+    },
+
+    {
+        title: 'Université du Québec à Montréal',
+        location:
+        {
+            lat: 45.512884,
+            lng:-73.558306
+        }
+    },
+
+    {
+        title: 'Dawson College',
+        location: 
+        {
+            lat: 45.489336,
+            lng: -73.586758
+        }
+    },
+
+    {
+        title: 'Collège LaSalle',
+        location:
+        {
+            lat: 45.491504,
+            lng: -73.581654
+        }
+    }
 ];
